7b4bd96641adfd0a9bc05013476efc5b8b116de8
Victor Jean <victorjean9@gmail.com>
2020-10-28 20:15:54 -0300

    Primeiro Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app-particulas">
+      <vue-particles color="#41B883"></vue-particles>
+    </div>
+    <div id="app-content" class="container">
+      <h1 id="logo-titulo">isFormula</h1>
+      <h6 id="logo-subtitulo">Verifica se uma sequência de caracteres é uma<br/>fórmula da Lógica Proposicional.</h6>
+      <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <div class="row no-gutters mt-3 align-items-center">
+        <!-- <input class="form-control border-secondary rounded-pill pr-5" type="search" placeholder="Digite aqui uma cadeia de caracteres." id="example-search-input2">
+        <button id="btn-go">-></button> -->
+        <div class="col">
+          <input class="form-control border-secondary rounded-pill pr-5" type="search" placeholder="Digite aqui uma cadeia de caracteres." id="example-search-input2">
+        </div>
+        <div class="col-auto">
+            <button class="btn btn-outline-light text-dark border-0 rounded-pill ml-n5" type="button">
+                <font-awesome-icon icon="arrow-right" />
+            </button>
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
 <style>
+@import './assets/styles/fontes.css';
+
+#logo-titulo {
+  font-family: VeganStyle;
+  font-size: 60px;
+}
+
+#logo-subtitulo {
+  font-weight: bold;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+#app-particulas {
+  position: fixed;
+  z-index: -1 !important;
+  width: 100%;
+  height: 100vh;
+}
+
+#app-content {
+  padding: 300px;
+}
+
+#btn-go {
+  background-color: blue;
+  border-radius: 50%;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueParticles from 'vue-particles'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faArrowRight)
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(BootstrapVue)
+Vue.use(VueParticles)
 
 Vue.config.productionTip = false
+ 
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
+
 
 new Vue({
   render: h => h(App),
